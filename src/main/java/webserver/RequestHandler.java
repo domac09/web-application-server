9318355f28ef5d640b562b607b773eaf00405a9c
woowahanlee <leesh@woowahan.com>
2017-04-10 00:01:25 +0900

    parse Querys.. 하지만 이미 메소드가 존재 했다는거..

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,15 @@
 package webserver;
 
-import java.io.*;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.util.Map;
-
-import com.sun.tools.javac.util.StringUtils;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.IOUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
 import static util.HttpRequestUtils.parseQueryString;
 
@@ -25,78 +25,89 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
 
-        // stream의 경우 try 구문에 선언을 하면 Closeable interface의 close구문이 자동으로 실행된다. jdk 1.7 문법
+        // stream의 경우 try 구문에 선언을 하면 Closeable interface의 close 구문이 자동으로 실행된다. jdk 1.7 문법
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 
             String line = br.readLine();
-            log.debug("request line = {}", line);
 
             if (line == null) {
                 return;
             }
 
-            String[] tokens = line.split(" ");
-            String url = tokens[1];
-            int contentLength = 0;
+            log.debug("line => {}", line);
+
+            String path = getPath(line);
 
             while (!"".equals(line)) {
                 line = br.readLine();
-                log.debug("header : {}", line);
-
-                if (line.contains("Content-Length")) {
-                    contentLength = getContentLength(line);
-                }
+                log.debug("header => {}", line);
             }
 
-            if (url.startsWith("/user/create")) {
-                String body = IOUtils.readData(br, contentLength);
-                User user = createUser(parseQueryString(body));
-                log.debug("User is = {}", user);
+            // user cleare logic
+            String[] params = path.split("\\?");
 
-                DataOutputStream dos = new DataOutputStream(out);
-                response302Header(dos, "/index.html");
+
+            log.debug("params => {}", params[1]);
+            String[] userValue = params[1].split("&");
+            Map<String, String> map = new HashMap<>();
+
+            for (String args : userValue) {
+                String[] splitParams = args.split("=");
+                map.put(splitParams[0], splitParams[1]);
             }
 
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            log.debug("converted map of params => {}", map);
+
 
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+
+            log.debug("path => {}", path);
+            int index = path.lastIndexOf(".");
+            String extension = path.substring(index + 1);
+
+            response200Header(dos, body.length, extension);
+
             responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private int getContentLength(String line) {
+    private String getPath(String line) {
         String[] split = line.split(" ");
-        return Integer.parseInt(split[1]);
+        return split[1];
     }
 
-    private void response302Header(DataOutputStream dos, String location) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
-            dos.writeBytes("Location: " + location + " \r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String extension) {
 
-    private User createUser(Map<String, String> parseQueryString) {
-        return new User(parseQueryString.get("userId"), parseQueryString.get("password"), parseQueryString.get("name"), parseQueryString.get("email"));
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + getContentType(extension) + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getContentType(String extension) {
+        switch (extension) {
+            case "css":
+                return "text/css;charset=utf-8";
+            case "js":
+                return "text/javascript;charset=utf-8";
+            case "woff":
+                return "font/woff";
+            case "woff2":
+                return "font/woffw";
+            case "png":
+                return "image/png";
+        }
+        return "text/html;charset=utf-8";
     }
 
     private void responseBody(DataOutputStream dos, byte[] body) {
